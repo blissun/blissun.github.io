03432eeb9d9f8b53abc5a21d55b76efb5f8f067a
blissun <hello-ryan@kakao.com>
2021-02-22 03:37:46 +0900

    delete 핸들 변경

--- js/todo.js
+++ js/todo.js
@@ -6,26 +6,43 @@ let init_state = true;
 const FINISHED = 'finished';
 const TODOS = 'todos';
 
-let toDos = []; // pending ;;
+let todo_list = []; // pending ;;
+
 function make_todo_obj(todo) {
-  let id = 0;
-  if (toDos.length) {
-    id = toDos[toDos.length - 1].id + 1;
+  let id = 1;
+  if (todo_list.length) {
+    id = todo_list[todo_list.length - 1].id + 1;
   }
   return { text: todo, id: id };
 }
 
 let finished = []; // finished ;;
 function make_finish_obj(todo) {
-  let id = 0;
+  let id = 1;
   if (finished.length) {
-    id = toDos[finished.length - 1].id + 1;
+    id = finished[finished.length - 1].id + 1;
   }
   return { text: todo, id: id };
 }
 
 function delete_handle(event) {
+  const parent_id = event.target.parentNode.parentNode.id;
+  const todo_id = event.target.parentNode.id;
   event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+  if (parent_id === 'pending_list') {
+    todo_list = todo_list.filter((todo) => {
+      return todo.id !== parseInt(todo_id);
+    });
+    localStorage.setItem(TODOS, JSON.stringify(todo_list));
+  } else {
+    console.log(finished);
+    console.log(todo_id);
+    finished = finished.filter((todo) => {
+      return todo.id !== parseInt(todo_id);
+    });
+    console.log(finished);
+    localStorage.setItem(FINISHED, JSON.stringify(finished));
+  }
 }
 
 function move_to_pending(event) {
@@ -43,13 +60,13 @@ function move_to_finished(event) {
   add_list(content, 'finished');
   event.target.parentNode.parentNode.removeChild(event.target.parentNode);
   const todo_id = event.target.parentNode.id;
-  toDos = toDos.filter((todo) => {
+  todo_list = todo_list.filter((todo) => {
     return todo.id !== parseInt(todo_id);
   });
-  localStorage.setItem(TODOS, JSON.stringify(toDos));
+  localStorage.setItem(TODOS, JSON.stringify(todo_list));
 }
 
-function add_list(todo, kind) {
+function add_list(todo, kind, id) {
   const del_btn = document.createElement('span');
   const li = document.createElement('li');
   const span = document.createElement('span');
@@ -64,18 +81,26 @@ function add_list(todo, kind) {
     finish_btn.addEventListener('click', move_to_finished);
     li.appendChild(finish_btn);
     const todoObj = make_todo_obj(todo);
-    li.id = todoObj.id;
+    if (id) {
+      li.id = id;
+    } else {
+      li.id = todoObj.id;
+    }
     if (!init_state) {
-      toDos.push(todoObj);
+      todo_list.push(todoObj);
     }
     pending_list.appendChild(li);
-    localStorage.setItem(TODOS, JSON.stringify(toDos));
+    localStorage.setItem(TODOS, JSON.stringify(todo_list));
   } else {
     finish_btn.innerHTML = '👀';
     finish_btn.addEventListener('click', move_to_pending);
     li.appendChild(finish_btn);
     const todoObj = make_finish_obj(todo);
-    li.id = todoObj.id;
+    if (id) {
+      li.id = id;
+    } else {
+      li.id = todoObj.id;
+    }
     if (!init_state) {
       finished.push(todoObj);
     }
@@ -84,11 +109,12 @@ function add_list(todo, kind) {
   }
   del_btn.onclick = delete_handle;
   del_btn.innerHTML = '❌';
+  del_btn.classList.add('btn');
+  finish_btn.classList.add('btn');
   input_task.value = '';
 }
 
 function handle_submit(event) {
-  console.log(event);
   event.preventDefault();
   add_list(input_task.value, 'pending');
 }
@@ -98,14 +124,19 @@ form.addEventListener('submit', handle_submit);
 function init() {
   const todos_json = JSON.parse(localStorage.getItem(TODOS));
   const finished_json = JSON.parse(localStorage.getItem(FINISHED));
-  toDos = todos_json;
-  finished = finished_json;
-  todos_json.forEach((element) => {
-    add_list(element.text, 'pending');
-  });
-  finished_json.forEach((element) => {
-    add_list(element.text);
-  });
+
+  if (todos_json) {
+    todo_list = todos_json;
+    todos_json.forEach((element) => {
+      add_list(element.text, 'pending', element.id);
+    });
+  }
+  if (finished_json) {
+    finished = finished_json;
+    finished_json.forEach((element) => {
+      add_list(element.text, 'finished', element.id);
+    });
+  }
 
   init_state = false;
 }
